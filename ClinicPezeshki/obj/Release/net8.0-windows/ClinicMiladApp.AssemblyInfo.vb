95d5af4dbde58463ee76b1aa201e7c68e66b67df
Milad Tahanian <148430759+miladtahanian@users.noreply.github.com>
2025-08-04 13:10:26 +0330

    update

--- ClinicPezeshki/obj/Release/net8.0-windows/ClinicMiladApp.AssemblyInfo.vb
+++ ClinicPezeshki/obj/Release/net8.0-windows/ClinicMiladApp.AssemblyInfo.vb
@@ -14,7 +14,7 @@ Option Explicit On
 Imports System
 Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("ClinicMiladApp"),  _
- Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
+ Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
  Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db46aa204374de79e65e1cfd189b484931414dcb"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("ClinicMiladApp"),  _
